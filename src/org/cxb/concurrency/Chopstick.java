48a8afe6f909524e378227a281db4493b69296b8
Xubiao Chen <ischenxb@gmail.com>
2017-06-08 17:07:29 +0800

    20170608

---
+++ src/org/cxb/concurrency/Chopstick.java
@@ -0,0 +1,15 @@
+package org.cxb.concurrency;
+
+public class Chopstick {
+	private boolean taken = false;
+	public synchronized void take() throws InterruptedException {
+		while(taken)
+			wait();
+		taken = true;
+	}
+	
+	public synchronized void drop() {
+		taken = false;
+		notifyAll();
+	}
+}
